4ef8ca85651dadc4115937f050d2ecb6aafc3f7f
hsgamer <huynhqtienvtag@gmail.com>
2020-12-15 11:17:20 +0700

    do it again

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
 # ItemGotcha
+
 Commands and Requirements related to Item
 
 ## config.yml
+
 ***
+
 * **`config.yml` is a special file. It contains all the icons that are used in `give:` and `Item Requirement`**
+
 ***
+
 ### Info
+
 * Is a Dummy Menu
 * Can be opened by using `/items`
 * All the icons in this file are Dummy Icons.
+
 ### Usage
+
 * Similar settings as other menus
+
 ```yaml
 stone:
   id: STONE
@@ -23,32 +32,46 @@ advanced_stone:
   lore:
   - "A dummy Stone"
 ```
+
 ***
+
 ## Addon Command
+
 | Command | Permission | Description |
 | --- | --- | --- |
 | `items` | bettergui.items | Open the inventory that contains all items from config.yml |
 | `giveitem` | bettergui.items | Give an item to the player |
+
 ***
+
 ## Command Type
+
 | Command | Description | Argument | Example |
 | ------- | ----------- | -------- | ------- |
 | `give: <item>` | Give the item to the player | `<item>` <br> use the format `<material>` <br> or `<material>, <amount>` <br> where `<material>` is the item in `config.yml` or the [Material](https://hub.spigotmc.org/javadocs/spigot/org/bukkit/Material.html) | `give: test` <br> <br> `give: test, 10`  <br> <br> `give: DIAMOND_SWORD` <br> <br> `give: STONE, 10`
+
 ***
+
 ## Requirement Type
+
 ### Item
+
 * **Keyword**: `item`
 * **Value Type**: `String` or `List Of String`
 * **Final Value**: `List Of String` (names of icons from config.yml)
 * **Default Take**: `true`
 * **Note**
-  * There are two methods of checking required items:
-    * Old: The plugin will convert the icon to the final item and check that item with the player's inventory
-    * New: The plugin will look through the player's inventory. For each item, The plugin first check the material, then get all item properties and check each of them with the item, and finally check the amount.
-  * You can define the amount needed by using the format `<item>, <format>`
-  * You can enable the new method by adding `:false` after the value (Ex: `stone:false` or `stone, 10:false`, which `stone` is an icon from config.yml)
+    * There are two methods of checking required items:
+        * Old: The plugin will convert the icon to the final item and check that item with the player's inventory
+        * New: The plugin will look through the player's inventory. For each item, The plugin first check the material,
+          then get all item properties and check each of them with the item, and finally check the amount.
+    * You can define the amount needed by using the format `<item>, <format>`
+    * You can enable the new method by adding `:false` after the value (Ex: `stone:false` or `stone, 10:false`,
+      which `stone` is an icon from config.yml)
 * **Examples**
+
 ***
+
 ```yaml
 item: "stone"
 
@@ -64,10 +87,14 @@ item:
   - "cobblestone"
   take: false
 ```
+
 ***
+
 ## Icon Type
+
 * This addon also gives an icon type called `itemgotcha`, which represents the item from ItemGotcha's config
 * Example on a menu:
+
 ```yaml
 test-itemgotcha:
   slot: 1
@@ -75,4 +102,5 @@ test-itemgotcha:
   icon: stone # name of the item from the ItemGotcha config
   command: "telll: &aI'm a stone"
 ```
+
 ***
--- src/main/java/me/hsgamer/bettergui/itemgotcha/Main.java
+++ src/main/java/me/hsgamer/bettergui/itemgotcha/Main.java
@@ -1,113 +1,25 @@
 package me.hsgamer.bettergui.itemgotcha;
 
-import static me.hsgamer.bettergui.BetterGUI.getInstance;
-
-import java.util.ArrayList;
-import me.hsgamer.bettergui.builder.CommandBuilder;
-import me.hsgamer.bettergui.builder.IconBuilder;
-import me.hsgamer.bettergui.builder.RequirementBuilder;
-import me.hsgamer.bettergui.config.MessageConfig;
-import me.hsgamer.bettergui.object.addon.Addon;
-import me.hsgamer.bettergui.object.icon.DummyIcon;
-import me.hsgamer.bettergui.util.MessageUtils;
-import me.hsgamer.bettergui.util.PermissionUtils;
-import me.hsgamer.bettergui.util.config.path.StringConfigPath;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.defaults.BukkitCommand;
-import org.bukkit.entity.Player;
+import me.hsgamer.bettergui.api.addon.BetterGUIAddon;
+import me.hsgamer.bettergui.lib.core.bukkit.utils.PermissionUtils;
+import me.hsgamer.bettergui.lib.core.config.path.StringConfigPath;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 
-public final class Main extends Addon {
+import static me.hsgamer.bettergui.BetterGUI.getInstance;
 
-  public static final StringConfigPath INVALID_ITEM = new StringConfigPath(
-      "invalid-item", "&cUnable to get required item. Inform the staff");
-  public static final StringConfigPath ITEM_REQUIRED = new StringConfigPath(
-      "item-required", "&cYou should specify an item");
+public final class Main extends BetterGUIAddon {
 
-  public static final Permission PERMISSION = PermissionUtils
-      .createPermission(getInstance().getName().toLowerCase() + ".items", null,
-          PermissionDefault.OP);
-  private static ItemManager itemManager;
+    public static final StringConfigPath INVALID_ITEM = new StringConfigPath("invalid-item", "&cUnable to get required item. Inform the staff");
+    public static final StringConfigPath ITEM_REQUIRED = new StringConfigPath("item-required", "&cYou should specify an item");
+    public static final Permission PERMISSION = PermissionUtils.createPermission(getInstance().getName().toLowerCase() + ".items", null, PermissionDefault.OP);
 
-  public static ItemManager getItemManager() {
-    return itemManager;
-  }
-
-  @Override
-  public boolean onLoad() {
-    setupConfig();
-    INVALID_ITEM.setConfig(getInstance().getMessageConfig());
-    ITEM_REQUIRED.setConfig(getInstance().getMessageConfig());
-    getInstance().getMessageConfig().saveConfig();
-    return true;
-  }
-
-  @Override
-  public void onEnable() {
-    itemManager = new ItemManager(this);
-
-    RequirementBuilder.register(ItemRequirement::new, "item");
-    CommandBuilder.register(ItemCommand::new, "give:");
-    IconBuilder.register(IconType::new, "itemgotcha");
-
-    registerCommand(new BukkitCommand("items",
-        "Open the inventory that contains all items from items.yml", "/items",
-        new ArrayList<>()) {
-      @Override
-      public boolean execute(CommandSender commandSender, String s, String[] strings) {
-        if (!(commandSender instanceof Player)) {
-          MessageUtils.sendMessage(commandSender, MessageConfig.PLAYER_ONLY.getValue());
-          return false;
-        }
-        itemManager.createMenu((Player) commandSender);
+    @Override
+    public boolean onLoad() {
+        setupConfig();
+        INVALID_ITEM.setConfig(getInstance().getMessageConfig());
+        ITEM_REQUIRED.setConfig(getInstance().getMessageConfig());
+        getInstance().getMessageConfig().saveConfig();
         return true;
-      }
-    });
-    registerCommand(new BukkitCommand("giveitem",
-        "Give an item to the player", "/giveitem <item_name>",
-        new ArrayList<>()) {
-      @Override
-      public boolean execute(CommandSender commandSender, String s, String[] strings) {
-        if (!(commandSender instanceof Player)) {
-          MessageUtils.sendMessage(commandSender, MessageConfig.PLAYER_ONLY.getValue());
-          return false;
-        }
-        if (!commandSender.hasPermission(PERMISSION)) {
-          MessageUtils.sendMessage(commandSender, MessageConfig.NO_PERMISSION.getValue());
-          return false;
-        }
-        if (strings.length <= 0) {
-          MessageUtils.sendMessage(commandSender, ITEM_REQUIRED.getValue());
-          return false;
-        }
-        DummyIcon icon = itemManager.getMenu().getIcons().get(strings[0]);
-        if (icon != null) {
-          ((Player) commandSender).getInventory().addItem(icon.createClickableItem(
-              (Player) commandSender).get().getItem());
-          MessageUtils.sendMessage(commandSender, MessageConfig.SUCCESS.getValue());
-        } else {
-          MessageUtils.sendMessage(commandSender, INVALID_ITEM.getValue());
-          return false;
-        }
-        return true;
-      }
-    });
-  }
-
-  @Override
-  public void onPostEnable() {
-    itemManager.initializeMenu();
-  }
-
-  @Override
-  public void onDisable() {
-    itemManager.closeMenu();
-  }
-
-  @Override
-  public void onReload() {
-    reloadConfig();
-    itemManager.reload();
-  }
+    }
 }

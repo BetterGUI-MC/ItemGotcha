95552a517443a4133762fb20c3ee49225b4cafd9
Huynh Tien <huynhqtienvtag@gmail.com>
2021-07-08 11:09:10 +0700

    GiveItemCommand

---
+++ src/main/java/me/hsgamer/bettergui/itemgotcha/GiveItemCommand.java
@@ -0,0 +1,56 @@
+package me.hsgamer.bettergui.itemgotcha;
+
+import me.hsgamer.bettergui.Permissions;
+import me.hsgamer.bettergui.config.MessageConfig;
+import me.hsgamer.bettergui.lib.core.bukkit.utils.MessageUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public class GiveItemCommand extends Command {
+    private static final Permission PERMISSION = new Permission(Permissions.PREFIX + ".items", null, PermissionDefault.OP);
+
+    public GiveItemCommand() {
+        super("giveitem", "Give an item to the player", "/giveitem <item_name>", Collections.emptyList());
+        setPermission(PERMISSION.getName());
+    }
+
+    @Override
+    public boolean execute(CommandSender commandSender, String s, String[] strings) {
+        if (!testPermission(commandSender)) {
+            return false;
+        }
+        if (!(commandSender instanceof Player)) {
+            MessageUtils.sendMessage(commandSender, MessageConfig.PLAYER_ONLY.getValue());
+            return false;
+        }
+        if (strings.length <= 0) {
+            MessageUtils.sendMessage(commandSender, Main.ITEM_REQUIRED.getValue());
+            return false;
+        }
+        Optional<RequiredItem> optional = Manager.getRequiredItem(strings[0]);
+        if (optional.isPresent()) {
+            ((Player) commandSender).getInventory().addItem(optional.get().getItemStack((Player) commandSender, null));
+            MessageUtils.sendMessage(commandSender, MessageConfig.SUCCESS.getValue());
+        } else {
+            MessageUtils.sendMessage(commandSender, Main.INVALID_ITEM.getValue());
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public List<String> tabComplete(CommandSender commandSender, String s, String[] strings) {
+        if (strings.length == 1) {
+            return new ArrayList<>(Manager.getRequiredItemMap().keySet());
+        }
+        return super.tabComplete(commandSender, s, strings);
+    }
+}
--- src/main/java/me/hsgamer/bettergui/itemgotcha/Main.java
+++ src/main/java/me/hsgamer/bettergui/itemgotcha/Main.java
@@ -3,22 +3,11 @@ package me.hsgamer.bettergui.itemgotcha;
 import me.hsgamer.bettergui.api.addon.BetterGUIAddon;
 import me.hsgamer.bettergui.builder.ActionBuilder;
 import me.hsgamer.bettergui.builder.RequirementBuilder;
-import me.hsgamer.bettergui.config.MessageConfig;
-import me.hsgamer.bettergui.lib.core.bukkit.utils.MessageUtils;
 import me.hsgamer.bettergui.lib.core.config.path.StringConfigPath;
 import me.hsgamer.bettergui.lib.core.variable.VariableManager;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.defaults.BukkitCommand;
 import org.bukkit.entity.Player;
-import org.bukkit.permissions.Permission;
-import org.bukkit.permissions.PermissionDefault;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
 
 import static me.hsgamer.bettergui.BetterGUI.getInstance;
 
@@ -28,8 +17,7 @@ public final class Main extends BetterGUIAddon {
     public static final StringConfigPath ITEM_REQUIRED = new StringConfigPath("item-required", "&cYou should specify an item");
     public static final StringConfigPath CHECK_ITEM_SUCCESS = new StringConfigPath("check-item-success", "&a✓");
     public static final StringConfigPath CHECK_ITEM_FAILED = new StringConfigPath("check-item-failed", "&c✗");
-    public static final Permission PERMISSION = new Permission(getInstance().getName().toLowerCase() + ".items", null, PermissionDefault.OP);
-    private Command giveCommand;
+    private final Command giveCommand = new GiveItemCommand();
 
     @Override
     public boolean onLoad() {
@@ -58,41 +46,6 @@ public final class Main extends BetterGUIAddon {
             }
             return RequiredItemExecute.get(original).map(requiredItemExecute -> requiredItemExecute.check(player) ? CHECK_ITEM_SUCCESS.getValue() : CHECK_ITEM_FAILED.getValue()).orElse(null);
         });
-
-        giveCommand = new BukkitCommand("giveitem", "Give an item to the player", "/giveitem <item_name>", Collections.emptyList()) {
-            @Override
-            public boolean execute(CommandSender commandSender, String s, String[] strings) {
-                if (!(commandSender instanceof Player)) {
-                    MessageUtils.sendMessage(commandSender, MessageConfig.PLAYER_ONLY.getValue());
-                    return false;
-                }
-                if (!commandSender.hasPermission(PERMISSION)) {
-                    MessageUtils.sendMessage(commandSender, MessageConfig.NO_PERMISSION.getValue());
-                    return false;
-                }
-                if (strings.length <= 0) {
-                    MessageUtils.sendMessage(commandSender, ITEM_REQUIRED.getValue());
-                    return false;
-                }
-                Optional<RequiredItem> optional = Manager.getRequiredItem(strings[0]);
-                if (optional.isPresent()) {
-                    ((Player) commandSender).getInventory().addItem(optional.get().getItemStack((Player) commandSender, null));
-                    MessageUtils.sendMessage(commandSender, MessageConfig.SUCCESS.getValue());
-                } else {
-                    MessageUtils.sendMessage(commandSender, INVALID_ITEM.getValue());
-                    return false;
-                }
-                return true;
-            }
-
-            @Override
-            public List<String> tabComplete(CommandSender commandSender, String s, String[] strings) {
-                if (strings.length == 1) {
-                    return new ArrayList<>(Manager.getRequiredItemMap().keySet());
-                }
-                return super.tabComplete(commandSender, s, strings);
-            }
-        };
         registerCommand(giveCommand);
     }
 
